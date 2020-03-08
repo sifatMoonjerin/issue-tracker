ff92adc5b6ccfb512141b23f4ffcde75a629e8c7
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-03-08 15:25:55 +0600

    strikegit add .!

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ function submitIssue(e) {
   let issues = [];
   if (localStorage.getItem('issues')){
     issues = JSON.parse(localStorage.getItem('issues'));
-    console.log(issues);
   }
   issues.push(issue);
   localStorage.setItem('issues', JSON.stringify(issues));
@@ -24,7 +23,7 @@ function submitIssue(e) {
 
 const closeIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find(issue => issue.id === id);
+  const currentIssue = issues.find(issue => parseInt(issue.id) === id);
   currentIssue.status = 'Closed';
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
@@ -45,14 +44,23 @@ const fetchIssues = () => {
   for (var i = 0; i < issues.length; i++) {
     const {id, description, severity, assignedTo, status} = issues[i];
 
+    let descriptionStyle;
+
+    if(status === 'Open'){
+      descriptionStyle = description;
+    } else{
+      descriptionStyle = `<strike>${description}</strike>`;
+    }
+
     issuesList.innerHTML +=   `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status}</span></p>
-                              <h3> ${description} </h3>
+                              <h3> ${descriptionStyle} </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
+    
   }
 }
